9f117e33ab715cf9986161d840d5d1bb00f3e9f5
mritunjay <mritunjay.kumar@delhivery.com>
2023-06-08 12:12:57 +0530

    [main introduced]

---
+++ main.py
@@ -0,0 +1,12 @@
+class Main:
+    def __init__(self):
+        self._context = 'this is main'
+        pass
+
+    def run(self):
+        print(self._context)
+
+
+if __name__ == '__main__':
+    main = Main()
+    main.run()
